782167dde6e76c921f697d454808041ab50e37f2
Jyoti Singh <jyotisingh13@gmail.com>
2023-08-15 12:15:09 +0530

    isAuth modify

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -1,15 +1,47 @@
-const isAuth = (req, res, next) => {
-  console.log('req',req)
-  console.log("REQUEST SESSION IN ISAUTH", req.session.isAuth);
-  console.log("REQUEST SESSION USER", req.session.user);
+const jwt = require('jsonwebtoken');
 
-    if (req.session.isAuth) {
-      // User is authenticated, proceed to the next middleware or route handler
-      next();
-    } else {
-      // User is not authenticated, pass an error to the error-handling middleware
-      next(new Error("Invalid Session, Please login Again"));
-    }
+const isAuth = (req, res, next) => {
+  // Get the JWT token from the request header
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization token missing' });
+  }
+
+  try {
+    // Verify the JWT token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach the decoded user data to the request
+    req.user = decoded;
+
+    // User is authenticated, proceed to the next middleware or route handler
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
 };
 
 module.exports = { isAuth };
+
+
+
+
+
+
+
+// const isAuth = (req, res, next) => {
+//   console.log('req',req)
+//   console.log("REQUEST SESSION IN ISAUTH", req.session.isAuth);
+//   console.log("REQUEST SESSION USER", req.session.user);
+
+//     if (req.session.isAuth) {
+//       // User is authenticated, proceed to the next middleware or route handler
+//       next();
+//     } else {
+//       // User is not authenticated, pass an error to the error-handling middleware
+//       next(new Error("Invalid Session, Please login Again"));
+//     }
+// };
+
+// module.exports = { isAuth };
